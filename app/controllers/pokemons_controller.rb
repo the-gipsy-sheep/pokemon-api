06175f379dab7906faf55203a03517433ab3532a
Demian Yovanovich <subwaydemiany@gmail.com>
2022-12-09 10:20:01 -0300

    creado el controller/rutas

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,15 @@
+class PokemonsController < ApplicationController
+  def index
+    @pokemons = Pokemon.all
+    render json: @pokemons.to_json(only: %i[name type1 type2 number description image_url]), status: 200
+  end
+
+  def show
+    @pokemon = Pokemon.find_by_id(params[:id])
+    if @pokemon
+      render json: @pokemons.to_json(only: %i[name type1 type2 number description image_url]), status: 200
+    else
+      render json: {status: 404, message: 'Selecciona un Pokemon de la primera generación (1-151)'}, status: 404
+    end
+  end
+end
