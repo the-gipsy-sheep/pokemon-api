4ad24c4da1f7b1c38247d1ab9707a28af5e3b8bc
Demian Yovanovich <subwaydemiany@gmail.com>
2022-12-13 18:28:32 -0300

    solucionado un error en el show

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,15 +1,15 @@
 class PokemonsController < ApplicationController
   def index
     @pokemons = Pokemon.all
-    render json: @pokemons.to_json(only: %i[name type1 type2 number description image_url]), status: 200
+    render json: @pokemons.to_json(only: %i[name type1 type2 number description image_url id]), status: 200
   end
 
   def show
     @pokemon = Pokemon.find_by_id(params[:id])
     if @pokemon
-      render json: @pokemons.to_json(only: %i[name type1 type2 number description image_url]), status: 200
+      render json: @pokemon.to_json(only: %i[name type1 type2 number description image_url id]), status: 200
     else
-      render json: {status: 404, message: 'Selecciona un Pokemon de la primera generación (1-151)'}, status: 404
+      render json: { status: 404, message: 'Selecciona un Pokemon de la primera generación (1-151)'}, status: 404
     end
   end
 end
